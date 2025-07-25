34a2974c6b9a2e52c723a1ea9fc811db12ba5fe7
radevden <radevden@gmail.com>
2025-07-24 18:55:45 -0600

    fixed and done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     
     return (
         <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Denisa Radevová</Link>
+                <NavLink className="navbar-brand" to="/">Denisa Radevová</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <Link  className="nav-link" aria-current="page" to="/">Home</Link>
-                        <Link  className="nav-link" to="/about">About</Link>
-                        <Link  className="nav-link" to="/contact">Contact</Link>
+                        <NavLink  className="nav-link" aria-current="page" to="/">Home</NavLink>
+                        <NavLink  className="nav-link" to="/about">About</NavLink>
+                        <NavLink  className="nav-link" to="/contact">Contact</NavLink>
                     </div>
                 </div>
             </div>
