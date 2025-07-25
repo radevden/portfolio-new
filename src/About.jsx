27c33081681b9b1bb0ffc73522614bcc7a11d5b8
radevden <radevden@gmail.com>
2025-07-24 20:25:11 -0600

    about me page

--- src/About.jsx
+++ src/About.jsx
@@ -3,26 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function About () {
     return (
-        <div>
+        <div class="aboutme">
             <h1>
                 About me:
             </h1>
             <h3>
-                 Hi, I am Denisa — Front-End Developer
+                 Hi, I am Denisa — Front-End Developer.
                  <br/>I build responsive and interactive websites using React.
             </h3>
-            <h4>I am a front-end developer with a passion for clean, user-friendly interfaces. I’ve built projects with HTML, CSS, JavaScript, and React. Currently learning more about responsive design and improving accessibility.</h4>
+            <div class="row">
+                <div class="col-sm-6 mb-4">
+                    <h4 class="abouth4">I am a front-end developer with a passion for clean, user-friendly interfaces. I’ve built projects with HTML, CSS, JavaScript, and React. Currently learning more about responsive design and improving accessibility.</h4>
 
-            <h3>Skills:</h3>
-            <h4>
-                <ul>
-                    <li>
-                    Languages: HTML, CSS, JavaScript
-                    </li>
-                    <li>Frameworks: React, Bootstrap</li>
-                    <li>Tools: Git, VS Code, Figma</li>
-                </ul>
-            </h4>
+                    <h4>
+                        <ul>
+                            <li>
+                            Languages: HTML, CSS, JavaScript
+                            </li>
+                            <li>Frameworks: React, Bootstrap</li>
+                            <li>Tools: Git, VS Code, Figma</li>
+                        </ul>
+                    </h4>
+                </div>
+                <div class="col-sm-6 mb-4">
+                    <img src="" alt="me" />
+                </div>     
+            </div>
         </div>
     );
 }
