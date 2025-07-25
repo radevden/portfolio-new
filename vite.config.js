9dda5fe39d69e51fab81bf43b8dc35c4fc27feac
radevden <radevden@gmail.com>
2025-07-24 20:00:11 -0600

    troubleshooting 2.0

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/free-brands-svg-icons'
-      ]
-    }
-  }
 });
