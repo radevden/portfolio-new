20ce9b83ecefa1b6e1978955183401e9b3dfac5f
radevden <radevden@gmail.com>
2025-07-24 18:25:43 -0600

    fontawesome update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/', // Set this to match your deployment path
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/react-fontawesome']
+    }
+  }
 });
