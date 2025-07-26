4a9654362686ef413ddc18741cb6c8c27f0c107c
radevden <radevden@gmail.com>
2025-07-26 15:46:24 -0600

    troubleshooting

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -19,21 +19,21 @@ export default function Contact () {
     return (
         <div className="container-form">
             <h2 className="hcontact">Let's get in touch!</h2>
-            <form name="contact" method="POST" data-netlify="true" onClick={submitForm}>
-                <input type="hidden" name="forn-name" value="contact" />    
+            <form name="contact" method="POST" data-netlify="true" onSubmit={submitForm}>
+                <input type="hidden" name="form-name" value="contact" />    
                 <div className="mb-3">
                         <label for="nameInput" className="name-label">Name</label>
-                        <input type="text" className="name-form" id="nameInput" required/>
+                        <input name="name" type="text" className="name-form" id="nameInput" required/>
                     </div>
                     <div class="mb-3">
                         <label for="emailInput" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="emailInput" required/>
+                        <input type="email" name="email" className="form-control" id="emailInput" required/>
                     </div>
                     <div class="mb-3">
                         <label for="textArea" className="form-label">Your message</label>
-                        <textarea className="form-control" id="textArea" rows="3" required></textarea>
+                        <textarea className="form-control" name="textarea" id="textArea" rows="3" required></textarea>
                     </div>
-                    <input type="submit" className="contactbtn" value="Send"></input>
+                    <input type="submit" name="submit" className="contactbtn" value="Send"></input>
             </form>
         </div>
     );
